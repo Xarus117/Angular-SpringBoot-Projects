46038589b29701063e5d2b55c0e51c3c28803079
Cezary Gebczyk <74736356+Xarus117@users.noreply.github.com>
2023-09-28 18:24:44 +0200

    Add Employee Management Modals
    Added Bootstrap-based modals for creating, editing, and deleting employees in the Employee Manager app. Each modal includes form elements to interact with employee data, and they are linked to corresponding buttons on the employee cards. The modals are not yet functional and cannot be used to perform these actions on employee records.

--- EmployeeManager/employee-manager-frontend/src/app/app.component.ts
+++ EmployeeManager/employee-manager-frontend/src/app/app.component.ts
@@ -10,7 +10,7 @@ import { HttpErrorResponse } from '@angular/common/http';
 })
 export class AppComponent implements OnInit{
   title = ""
-  public employees: Employee[] = [];
+  public employees: Employee[] | undefined;
 
   constructor(private employeeService: EmployeeService) {}
 
@@ -27,5 +27,24 @@ export class AppComponent implements OnInit{
         alert(error.message);
       }
       );
+  }
+
+  public onOpenModal (employee?: Employee, mode?: string): void {
+    const container = document.getElementById('main-container');
+    const button = document.createElement('button');
+    button.type = 'button';
+    button.style.display = 'none';
+    button.setAttribute('data-toggle', 'modal');
+    if (mode === 'add') {
+      button.setAttribute('data-target', '#addEmployeeModal');
+    }
+    if (mode === 'edit') {
+      button.setAttribute('data-target', '#editEmployeeModal');
+    }
+    if (mode === 'delete') {
+      button.setAttribute('data-target', '#deleteEmployeeModal');
+    }
+    container?.appendChild(button);
+    button.click();
   }
 }
