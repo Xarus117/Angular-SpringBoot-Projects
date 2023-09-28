86ffab1d336dfa058d28535f941fd7a2da37eaf7
Cezary Gebczyk <74736356+Xarus117@users.noreply.github.com>
2023-09-28 15:46:04 +0200

    Enhanced EmployeeService with Exception Handling and Repository Methods
    In the EmployeeService class, I've made several enhancements. First, I added the generation of a unique employee code using UUID when adding a new employee. Second, I've incorporated exception handling by throwing a UserNotFoundException with a message when attempting to find an employee by ID that doesn't exist in the database. Additionally, I've implemented repository methods for updating and deleting employees.

--- EmployeeManager/employee-manager-backend/src/main/java/com/gebczyk/employeemanager/repo/EmployeeRepo.java
+++ EmployeeManager/employee-manager-backend/src/main/java/com/gebczyk/employeemanager/repo/EmployeeRepo.java
@@ -1,7 +1,10 @@
 package com.gebczyk.employeemanager.repo;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.gebczyk.employeemanager.model.Employee;
 
 public interface EmployeeRepo extends JpaRepository<Employee, Long> {
-    
+    void deleteEmployeeById(Long id);
+
+    Optional<Employee> findEmployeeById(Long id);
 }
--- EmployeeManager/employee-manager-backend/src/main/java/com/gebczyk/employeemanager/service/EmployeeService.java
+++ EmployeeManager/employee-manager-backend/src/main/java/com/gebczyk/employeemanager/service/EmployeeService.java
@@ -1,8 +1,10 @@
 package com.gebczyk.employeemanager.service;
+import java.util.List;
 import java.util.UUID;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.gebczyk.employeemanager.exception.UserNotFoundException;
 import com.gebczyk.employeemanager.model.Employee;
 import com.gebczyk.employeemanager.repo.EmployeeRepo;
 
@@ -18,5 +20,22 @@ public class EmployeeService {
     public Employee addEmployee(Employee employee) {
         employee.setEmployeeCode(UUID.randomUUID().toString());
         return employeeRepo.save(employee);
+    }
+
+    public List<Employee> findAllEmployees() {
+        return employeeRepo.findAll();
+    }
+
+    public Employee updatEmployee(Employee employee) {
+        return employeeRepo.save(employee);
+    }
+
+    public Employee findEmployeeById(Long id) {
+        return employeeRepo.findEmployeeById(id)
+                .orElseThrow(() -> new UserNotFoundException ("User by id " + id + " was not found"));
+    }
+
+    public void deleteEmployee(Long id) {
+        employeeRepo.deleteEmployeeById(id);
     }
 }
