89ab361d224b11d8764e2ea105543be52551a5ca
Cezary Gebczyk <74736356+Xarus117@users.noreply.github.com>
2023-09-28 15:24:21 +0200

    Added Employee Repository and Service
    Implemented an Employee repository extending JPA repositories to handle database operations for the Employee model. Additionally, introduced an EmployeeService with an 'addEmployee' method for inserting employee records.

---
+++ EmployeeManager/employee-manager-backend/src/main/java/com/gebczyk/employeemanager/service/EmployeeService.java
@@ -0,0 +1,22 @@
+package com.gebczyk.employeemanager.service;
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.gebczyk.employeemanager.model.Employee;
+import com.gebczyk.employeemanager.repo.EmployeeRepo;
+
+@Service
+public class EmployeeService {
+    private final EmployeeRepo employeeRepo;
+
+    @Autowired
+    public EmployeeService(EmployeeRepo employeeRepo) {
+        this.employeeRepo = employeeRepo;
+    }
+
+    public Employee addEmployee(Employee employee) {
+        employee.setEmployeeCode(UUID.randomUUID().toString());
+        return employeeRepo.save(employee);
+    }
+}
