b1106ba05a784704b3cef0392e4e8855678974fd
Cezary Gebczyk <74736356+Xarus117@users.noreply.github.com>
2023-09-28 17:12:49 +0200

    Employee Interface, AppComponent, and EmployeeService
    Set up the initial frontend structure for our employee management application. Created the Employee interface to define employee data structure, implemented the AppComponent to retrieve and display employee data using the EmployeeService. The EmployeeService was updated to facilitate API integration, including HTTP requests for retrieving, adding, updating, and deleting employee records.

--- EmployeeManager/employee-manager-frontend/src/app/app.component.spec.ts
+++ EmployeeManager/employee-manager-frontend/src/app/app.component.spec.ts
@@ -12,16 +12,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'employee-manager-front'`, () => {
+  it(`should have as title 'employee-manager-frontend'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('employee-manager-front');
+    expect(app.title).toEqual('employee-manager-frontend');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('employee-manager-front app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('employee-manager-frontend app is running!');
   });
 });
