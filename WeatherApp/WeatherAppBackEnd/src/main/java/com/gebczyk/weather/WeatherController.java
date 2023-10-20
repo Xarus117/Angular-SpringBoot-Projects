b5a9e530356d681df09547a0073cf2d5132de36f
Cezary Gebczyk <74736356+Xarus117@users.noreply.github.com>
2023-10-20 12:30:57 +0200

    Weather App
    Using: https://www.weatherapi.com/

---
+++ WeatherApp/WeatherAppBackEnd/src/main/java/com/gebczyk/weather/WeatherController.java
@@ -0,0 +1,26 @@
+package com.gebczyk.weather;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class WeatherController {
+    private final String apiKey = "19cbf39e2d05498a9bf100552232010";
+    private final RestTemplate restTemplate;
+    
+    @Autowired
+    public WeatherController(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
+
+    @GetMapping("/weather")
+    public String getWeather(@RequestParam String city) {
+        String apiUrl = "http://api.weatherapi.com/v1/current.json?key=" + apiKey + "&q=" + city;
+        String response = restTemplate.getForObject(apiUrl, String.class);
+        return response;
+    }
+}
---
+++ WeatherApp/WeatherAppBackEnd/src/test/java/com/gebczyk/weather/WeatherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gebczyk.weather;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
